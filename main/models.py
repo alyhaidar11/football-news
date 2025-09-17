d54ad2950972e90e168850997369b767ebd8aee6
alyhaidar11 <m.alyhaidarulloh@gmail.com>
2025-09-17 14:08:47 +0700

    penambahan fitur login,cookies, dan logout

--- main/models.py
+++ main/models.py
@@ -1,5 +1,6 @@
 import uuid
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -21,6 +22,8 @@ class News(models.Model):
     news_views = models.PositiveIntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
     is_featured = models.BooleanField(default=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True) 
+
     
     def __str__(self):
         return self.title
@@ -32,3 +35,5 @@ class News(models.Model):
     def increment_views(self):
         self.news_views += 1
         self.save()
+
+
