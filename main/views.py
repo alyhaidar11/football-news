01b8977231dd5c0781f80e4e58eacc3bc0ea188f
alyhaidar11 <m.alyhaidarulloh@gmail.com>
2025-09-24 14:02:41 +0700

    change directory path

--- main/views.py
+++ main/views.py
@@ -142,4 +142,4 @@ def edit_news(request, id):
 def delete_news(request, id):
     news = get_object_or_404(News, pk=id)
     news.delete()
-    return HttpResponseRedirect(reverse('main:show_main'))
+    return HttpResponseRedirect(reverse('main:show_main'))  
